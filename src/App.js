20bfca7f99b1f0cccb46dd61fb4f28567cd56bfd
Devin Brown <devin.g.brown@gmail.com>
2017-12-08 13:49:38 -0700

    working with mvp redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.props.counter}</h1>
+
+        <button onClick={this.handleIncrement}>+</button>
+        <button onClick={this.handleDecrement}>-</button>
       </div>
     );
+  }
+
+  handleIncrement = () => {
+    this.props.handleIncrement();
+  }
+
+  handleDecrement = () => {
+    this.props.handleDecrement();
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,43 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+
+/* create store */
+const store = createStore(counter);
+
+/* actions */
+function handleIncrement() {
+  store.dispatch({ type: 'INCREMENT' });
+};
+
+function handleDecrement(){
+  store.dispatch({ type: 'DECREMENT' });
+};
+
+/* reducer */
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state
+  }
+}
+
+function renderApp() {
+  ReactDOM.render(<App counter={store.getState()} handleIncrement={handleIncrement} handleDecrement={handleDecrement} />, document.getElementById('root'));
+};
+
+/* render on page load */
+renderApp();
+
+/* subscribe to data changes */
+store.subscribe(() => {
+  console.log('update')
+  renderApp()
+});
+
 registerServiceWorker();
