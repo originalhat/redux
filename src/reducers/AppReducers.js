7137a2bfb5535f96cec8e3234f1250c93b143f1a
Devin Brown <devin.g.brown@gmail.com>
2017-12-15 06:25:56 -0700

    break apart reducer

--- src/reducers/AppReducers.js
+++ src/reducers/AppReducers.js
@@ -3,12 +3,41 @@ const INITIAL_STATE = {name: 'devin', value: 0};
 export default function rootReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'UPDATE_NAME':
-      return Object.assign({...state}, {name: action.name});
+      return Object.assign(
+        {...state},
+        name(state, action)
+      );
     case 'INCREMENT_COUNTER':
-      return Object.assign({...state}, {value: state.value + 1});
+      return Object.assign(
+        {...state},
+        counter(state, action)
+      );
     case 'DECREMENT_COUNTER':
-      return Object.assign({...state}, {value: state.value - 1});
+      return Object.assign(
+        {...state},
+        counter(state, action)
+      );
     default:
-      return state
+      return state;
   }
 }
+
+const name = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_NAME':
+      return {name: action.name};
+    default:
+      return state;
+  }
+};
+
+const counter = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      return {value: state.value + 1};
+    case 'DECREMENT_COUNTER':
+      return {value: state.value - 1};
+    default:
+      return state;
+  }
+};
